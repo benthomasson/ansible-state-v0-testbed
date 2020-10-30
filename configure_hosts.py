567f17b6d8463be5abaec51c6830b5ee43d11149
Ben Thomasson <bthomass@redhat.com>
2020-10-30 08:40:23 -0400

    Extract script from expect

--- configure_hosts.py
+++ configure_hosts.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-
 """
 Usage:
     configure_hosts [options]
@@ -16,6 +15,7 @@ from docopt import docopt
 import logging
 import os
 import sys
+import yaml
 import pexpect
 from getpass import getpass
 
@@ -33,64 +33,73 @@ def main(args=None):
     else:
         logging.basicConfig(level=logging.WARNING)
 
-
     passwd = getpass()
 
     with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as f:
         key = f.read()
 
+    script = '''
+command: "sudo virsh console {host}"
+script:
+    - expect: "password for.*:"
+    - send: "{passwd}"
+    - send: "\\n"
+    - send: "\\n"
+    - expect: "Connected to domain"
+    - send: "\\n"
+    - expect: "login:"
+    - send: "root\\n"
+    - logfile: "{host}.log"
+    - expect: "#"
+    - send: "echo 'auto lo\\niface lo inet loopback\\nauto eth0\\niface eth0 inet dhcp\\n' > /etc/network/interfaces\\n"
+    - expect: "#"
+    - send: "apk add openssh\\n"
+    - expect: "#"
+    - send: "echo 'PermitRootLogin yes' >>  /etc/ssh/sshd_config\\n"
+    - expect: "#"
+    - send: "/etc/init.d/sshd start\\n"
+    - expect: "#"
+    - send: "/etc/init.d/networking restart\\n"
+    - expect: "#"
+    - send: "mkdir -p ~/.ssh\\n"
+    - expect: "#"
+    - send: "chmod 700 .ssh\\n"
+    - expect: "#"
+    - send: "echo '{key}' >> .ssh/authorized_keys\\n"
+    - expect: "#"
+    - send: "echo 'http://nl.alpinelinux.org/alpine/v3.12/main/' >> /etc/apk/repositories\\n"
+    - expect: "#"
+    - send: "apk update\\n"
+    - expect: "#"
+    - send: "apk add python3\\n"
+    - expect: "#"
+    - send: "exit\\n"
+    '''
+
     for host in ['host1', 'host2']:
-        console = pexpect.spawn(f'sudo virsh console {host}')
-        console.expect(r'password for.*:')
-        console.send(passwd)
-        console.send('\n')
-        console.send('\n')
-        console.expect('Connected to domain')
-        console.send('\n')
-        console.expect('login:')
-        console.send('root\n')
-        #console.expect('Password:')
-        #console.send('1234\n')
-        with open(f'{host}.log', 'wb') as f:
-            console.logfile = f
-            console.expect(r'#')
-            console.send('\n')
-            console.expect(r'#')
-            console.send('echo "auto lo\niface lo inet loopback\nauto eth0\niface eth0 inet dhcp\n" > /etc/network/interfaces\n')
-            console.expect(r'#')
-            console.send('apk add openssh\n')
-            console.expect(r'#')
-            #console.send('passwd\n')
-            #console.expect(r'New password:')
-            #console.send('1234\n')
-            #console.expect(r'Retype password:')
-            #console.send('1234\n')
-            #console.expect(r'#')
-            console.send("echo 'PermitRootLogin yes' >>  /etc/ssh/sshd_config\n")
-            console.expect(r'#')
-            console.send('/etc/init.d/sshd start\n')
-            console.expect(r'#')
-            console.send('/etc/init.d/networking restart\n')
-            console.expect(r'#')
-            console.send('mkdir -p ~/.ssh\n')
-            console.expect(r'#')
-            console.send('chmod 700 .ssh\n')
-            console.expect(r'#')
-            console.send(f'echo "{key}" >> .ssh/authorized_keys\n')
-            console.expect(r'#')
-            console.send('chmod 600 .ssh/authorized_keys\n')
-            console.expect(r'#')
-            console.send("echo 'http://nl.alpinelinux.org/alpine/v3.12/main/' >> /etc/apk/repositories\n")
-            console.expect(r'#')
-            console.send("apk update\n")
-            console.expect(r'#')
-            console.send("apk add python3\n")
-            console.expect(r'#')
-            console.send('exit\n')
-            #console.interact()
+        loaded_script = yaml.safe_load(script.format(key=key, host=host, passwd=passwd))
+        console = pexpect.spawn(loaded_script['command'])
+
+        logfile = None
+
+        for line in loaded_script['script']:
+            line_type, line_value = list(line.items())[0]
+            print(line_type, line_value)
+            if line_type == "logfile":
+                logfile = open(line_value, 'wb')
+                console.logfile = logfile
+            elif line_type == "expect":
+                console.expect(line_value)
+            elif line_type == "send":
+                console.send(line_value)
+            elif line_type == "interact":
+                console.interact()
+
+        if logfile:
+            logfile.flush()
+            logfile.close()
     return 0
 
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
-
