5eb482e9f053f006fefb9938d1af3bcf38c194fb
Ben Thomasson <bthomass@redhat.com>
2020-10-29 20:51:14 -0400

    Extract expect script into yaml

--- configure_routers.py
+++ configure_routers.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-
 """
 Usage:
     configure_routers [options]
@@ -17,6 +15,7 @@ import logging
 import os
 import sys
 import pexpect
+import yaml
 from getpass import getpass
 
 logger = logging.getLogger('configure_routers')
@@ -33,55 +32,77 @@ def main(args=None):
     else:
         logging.basicConfig(level=logging.WARNING)
 
-
     passwd = getpass()
 
     with open(os.path.expanduser('~/.ssh/id_rsa.pub')) as f:
         key = f.read()
 
+    script = '''
+---
+command: "sudo virsh console {host}"
+script:
+    - expect: "password for.*:"
+    - send: "{passwd}"
+    - send: "\\n"
+    - send: "\\n"
+    - expect: "Connected to domain"
+    - send: "\\n"
+    - expect: "login:"
+    - send: "vyos\\n"
+    - expect: "Password:"
+    - send: "vyos\\n"
+    - logfile: "{host}.log"
+    - expect: "$"
+    - send: "\\n"
+    - expect: "$"
+    - send: "configure\\n"
+    - expect: "#"
+    - send: "set interfaces ethernet eth0 address dhcp\\n"
+    - expect: "#"
+    - send: "set service ssh port 22\\n"
+    - expect: "#"
+    - send: "commit\\n"
+    - expect: "#"
+    - send: "save\\n"
+    - expect: "#"
+    - send: "exit\\n"
+    - expect: "$"
+    - send: "mkdir -p ~/.ssh\\n"
+    - expect: "$"
+    - send: "chmod 700 .ssh\\n"
+    - expect: "$"
+    - send: "echo '{key}' >> .ssh/authorized_keys\\n"
+    - expect: "$"
+    - send: "chmod 600 .ssh/authorized_keys\\n"
+    - expect: "$"
+    - send: "exit\\n"
+...
+    '''
+
     for host in ['vyos_r1', 'vyos_r2']:
-        console = pexpect.spawn(f'sudo virsh console {host}')
-        console.expect(r'password for.*:')
-        console.send(passwd)
-        console.send('\n')
-        console.send('\n')
-        console.expect('Connected to domain')
-        console.send('\n')
-        console.expect('login:')
-        console.send('vyos\n')
-        console.expect('Password:')
-        console.send('vyos\n')
-        with open(f'{host}.log', 'wb') as f:
-            console.logfile = f
-            console.expect(r'$')
-            console.send('\n')
-            console.expect(r'$')
-            console.send('configure\n')
-            console.expect(r'#')
-            console.send('set interfaces ethernet eth0 address dhcp\n')
-            console.expect(r'#')
-            console.send('set service ssh port 22\n')
-            console.expect(r'#')
-            console.send("commit\n")
-            console.expect(r'#')
-            console.send('save\n')
-            console.expect(r'#')
-            console.send('exit\n')
-            console.expect(r'$')
-            console.send('mkdir -p ~/.ssh\n')
-            console.expect(r'$')
-            console.send('chmod 700 .ssh\n')
-            console.expect(r'$')
-            console.send(f'echo "{key}" >> .ssh/authorized_keys\n')
-            console.expect(r'$')
-            console.send('chmod 600 .ssh/authorized_keys\n')
-            console.expect(r'$')
-            console.send('exit\n')
-            #console.interact()
+        loaded_script = yaml.safe_load(script.format(key=key, host=host, passwd=passwd))
+        console = pexpect.spawn(loaded_script['command'])
+
+        logfile = None
+
+        for line in loaded_script['script']:
+            line_type, line_value = list(line.items())[0]
+            print(line_type, line_value)
+            if line_type == "logfile":
+                logfile = open(line_value, 'wb')
+                console.logfile = logfile
+            elif line_type == "expect":
+                console.expect(line_value)
+            elif line_type == "send":
+                console.send(line_value)
+            elif line_type == "interact":
+                console.interact()
+
+        if logfile:
+            logfile.flush()
+            logfile.close()
     return 0
 
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
-
-
